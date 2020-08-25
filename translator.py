31ed9d514729c9af6062768cf38b0323ceacc0fe
imfulee <40764288+imfulee@users.noreply.github.com>
2020-08-25 15:26:13 +0800

    Initial commit
    has the basic files for operation

---
+++ translator.py
@@ -0,0 +1,17 @@
+import os
+import re
+import subprocess
+import platform
+
+# Checking the operating system
+# Also provide the correct Macros
+if os.name == 'posix':
+    ape = './ape.exe'
+elif os.name == 'nt':
+    ape = 'ape.exe'
+
+# Takes the text from nl.txt (stands for natural language) and output to text.txt
+if __name__ == "__main__":
+    file = open("text.owl", "w")
+    command = subprocess.run([ape, '-file', 'nl.txt', '-solo', 'owlxml', '-guess'], stdout=file)
+    file.write(str(command.stdout))
